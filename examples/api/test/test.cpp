58816e49f3c81533f5ed05c13014fc872aeccfd2
Bert Schiettecatte <bert@noisetron.com>
2022-03-02 15:04:53 -0800

    wip on demo code;

--- examples/api/test/test.cpp
+++ examples/api/test/test.cpp
@@ -8,20 +8,9 @@
 
 class TestPluginEditorInterface : public Percussa::SSP::PluginEditorInterface {
 public: 	
-	unsigned int VBO, VAO;
-	unsigned int shaderProgram;
-
-	float x = 0.0f; 
-	float y = 0.0f; 
-	float z = 1.0f; 
-	float w = 0.5f;
-	float h = 0.5f; 
-
-	float vertices[3][3] = {
-		{ x,		y + h,	z },
-		{ x,		y,	z },
-		{ x + w,	y,	z },
-	}; 
+	GLuint VAO = 0; 
+	GLuint VBO = 0; 
+	unsigned int shaderProgram = 0; 
 
 	static constexpr float projXmin   =  0.0f;
 	static constexpr float projXmax   =  1.0f;
@@ -30,9 +19,17 @@ public:
 	static constexpr float projZmin   = -1.0f;
 	static constexpr float projZmax   =  1.0f;
 
+	float z = projZmax; 
+	float w = (projXmax-projXmin)*0.1f; 
+	float h = (projYmax-projYmin)*0.1f;
+	float midx = (projXmax-projXmin)*0.5f;
+	float midy = (projYmax-projYmin)*0.5f;
+	float x = 0;
+	float y = 0;
+
 	glm::mat4 proj = glm::ortho(
-		projXmin,
-		projXmax,
+		projXmin-0.25f,
+		projXmax+0.25f,
 		projYmin,
 		projYmax,
 		projZmin,
@@ -42,11 +39,14 @@ public:
 	glm::mat4 view = glm::mat4(1.0f);
 	glm::mat4 model = glm::mat4(1.0f);
 
+	GLint mLoc = 0; 
+	GLint vLoc = 0; 
+	GLint pLoc = 0; 
 
 public:
 	TestPluginEditorInterface() {
 
-		const char* vertexShaderSource = R"(
+		static const char* vertexShaderSource = R"(
 			#version 300 es
 			layout (location = 0) in vec3 aPos;
 			uniform mat4 model; 
@@ -57,12 +57,12 @@ public:
 			}
 		)";
 
-		const char* fragmentShaderSource = R"(
+		static const char* fragmentShaderSource = R"(
 			#version 300 es
 			precision highp float; 
 			out vec4 FragColor;
 			void main() { 
-				FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+				FragColor = vec4(1.0, 1.0, 1.0, 1.0);
 			}
 		)";
 
@@ -100,7 +100,7 @@ public:
 		}
 
 		// link shaders
-		unsigned int shaderProgram = glCreateProgram();
+		shaderProgram = glCreateProgram();
 		glAttachShader(shaderProgram, vertexShader);
 		glAttachShader(shaderProgram, fragmentShader);
 		glLinkProgram(shaderProgram);
@@ -121,12 +121,16 @@ public:
 
 		// set proj/view/model matrices in shader 
 		glUseProgram(shaderProgram);
-		glUniformMatrix4fv(glGetUniformLocation(
-			shaderProgram, "proj"), 1, false, glm::value_ptr(proj)); 
-		glUniformMatrix4fv(glGetUniformLocation(
-			shaderProgram, "view"), 1, false, glm::value_ptr(view)); 
-		glUniformMatrix4fv(glGetUniformLocation(
-			shaderProgram, "model"), 1, false, glm::value_ptr(model)); 
+
+		mLoc = glGetUniformLocation(shaderProgram, "model");
+		vLoc = glGetUniformLocation(shaderProgram, "view");
+		pLoc = glGetUniformLocation(shaderProgram, "proj");
+
+		view = glm::translate(view, glm::vec3(0.5f, 0.5f, 0.0f)); 
+
+		glUniformMatrix4fv(mLoc, 1, false, glm::value_ptr(model)); 
+		glUniformMatrix4fv(vLoc, 1, false, glm::value_ptr(view)); 
+		glUniformMatrix4fv(pLoc, 1, false, glm::value_ptr(proj)); 
 
 		std::clog << __FUNCTION__ << ": set proj/view/model matrices" << std::endl; 
 
@@ -143,7 +147,7 @@ public:
 
 		glBindBuffer(GL_ARRAY_BUFFER, VBO);
 		//glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_DYNAMIC_DRAW);
+		glBufferData(GL_ARRAY_BUFFER, 3*3*sizeof(float), NULL, GL_DYNAMIC_DRAW);
 
 		glEnableVertexAttribArray(0);
 		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
@@ -173,14 +177,63 @@ public:
 	void visibilityChanged(bool b) override {}
 	void renderToImage(unsigned char *buffer, int width, int height) override {}
 
+	float counter = 0.0f; 
+
 	// called after renderToImage, when all OpenGLES geometry is being drawn. 
 	void draw(int width, int height) override {
 
+		float ar = width;
+		ar /= height; 
+
+		//glClearColor(0.0f, 0.0f, counter, 1.0f);
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO); 
+
+		x = 0; 
+		y = 0; 
+		w = 0.25f; 
+		h = std::sqrt(3)/2*w; 
+		z = 1.0f; 
+
+		float vertices[3][3] = {
+			{ x,		y,		z },
+			{ x + w,	y,		z },
+			{ x + w*0.5f,	y + h,		z },
+		}; 
+
+		glBindBuffer(GL_ARRAY_BUFFER, VBO); 
+		glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices); 
+		glBindBuffer(GL_ARRAY_BUFFER, 0); 
+
+		// rotate model matrix by 1 degrees around z axis and update in shader
+		model = glm::rotate(
+				glm::mat4(1.0f), 
+				glm::radians(counter), 
+				glm::vec3(0.0f, 0.0f, 1.0f)); 
+
+		glUniformMatrix4fv(mLoc, 1, false, glm::value_ptr(model)); 
+
+		proj = glm::ortho(
+			-ar, ar,
+			-1.0f, 1.0f, 
+			-1.0f, 1.0f 
+		); 
+
+		glUniformMatrix4fv(pLoc, 1, false, glm::value_ptr(proj)); 
+
+		// draw triangle 
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 		glBindVertexArray(0); 
-
+/*
+		x += 0.001f; 
+		y += 0.001f; 
+		if (x > projXmax) x -= projXmax; 
+		if (y > projYmax) y -= projYmax; 
+*/
+		counter += 1.0f; 
+		if (counter > 360.0f) 
+			counter -= 360.0f; 
+		//std::clog << __FUNCTION__ << ": counter: " << counter << std::endl; 
 		//throw std::runtime_error("stop"); 
 	}
 };
